27c7a35734f9a6f96ce83cb1e540b10b3fa0152e
phani-rec <phaneendra.allu@raghuenggcollege.in>
2025-01-07 15:46:46 +0530

    numpy install steps

---
+++ unit_1/01_array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# 1D ndarray
+arr1 = np.array([1, 2, 3, 4])
+
+# 2D ndarray (matrix)
+arr2 = np.array([[1, 2], [3, 4]])
+
+# 3D ndarray
+arr3 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+print(arr1)
+print(arr2)
+print(arr3)
