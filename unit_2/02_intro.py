837d81513c87940e5666f2fd799529a32dda501b
phani-rec <phaneendra.allu@raghuenggcollege.in>
2025-02-14 12:28:33 +0530

    pandas series added

---
+++ unit_2/02_intro.py
@@ -0,0 +1,8 @@
+import pandas as pd  
+import numpy as np  
+a = np.array(['R','a','g','h','u'])  
+b = pd.Series(a)  
+print("value in a -->",a)
+print("type of a -->",type(a))
+print("value in b -->",b)
+print("type of b -->",type(b))
